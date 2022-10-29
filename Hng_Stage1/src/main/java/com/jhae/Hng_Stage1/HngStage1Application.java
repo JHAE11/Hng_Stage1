9865e729cf11bcaf16ee6e6a84b931430e296be7
JHAE <Favourujah44@gmail.com>
2022-10-29 07:06:41 -0700

    rest mapping changes

--- Hng_Stage1/src/main/java/com/jhae/Hng_Stage1/HngStage1Application.java
+++ Hng_Stage1/src/main/java/com/jhae/Hng_Stage1/HngStage1Application.java
@@ -2,9 +2,22 @@ package com.jhae.Hng_Stage1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class HngStage1Application {
+
+	@GetMapping("/")
+	@ResponseBody
+	public ResponseEntity<Model> showUser() {
+		Model model = new Model();
+		return ResponseEntity.ok(model);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HngStage1Application.class, args);
